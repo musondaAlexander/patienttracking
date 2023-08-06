40bbcb88e88c80a5ce30e0817d0be264b1882e64
Alexander Musonda <musondaalexander97@gmail.com>
2023-08-06 12:52:08 +0200

    added New Project

---
+++ README.md
@@ -0,0 +1,1 @@
+Welcome to patient to Patient tracking app
---
+++ android/app/src/main/kotlin/com/example/patienttracking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.patienttracking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
