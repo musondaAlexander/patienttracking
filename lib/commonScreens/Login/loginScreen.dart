38a89f1cb0084766f662d4283bb17a4117bd7f49
Alexander Musonda <74776587+musondaAlexander@users.noreply.github.com>
2023-09-22 16:29:20 +0200

    added login page

---
+++ lib/commonScreens/Login/loginScreen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:patienttracking/commonScreens/formFooter.dart';
+import 'package:patienttracking/commonScreens/Login/loginForm.dart';
+
+class LogingScreen extends StatelessWidget {
+  const LogingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // image: AssetImage("images/logo.png"),
+    // This calculates the height of the Screen
+    double preferredHeight = MediaQuery.of(context).size.height * 0.1;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+        // bottom: PreferredSize(
+        //     preferredSize: Size.fromHeight(preferredHeight),
+        //     child: Container(
+        //       padding: const EdgeInsets.only(bottom: 10),
+        //       child: Column(
+        //         children: [
+        //           Image(
+        //             image: const AssetImage("assets/logos/logoicon.ico"),
+        //             height: preferredHeight,
+        //           ),
+        //           Text(
+        //             "Login",
+        //             style: TextStyle(
+        //                 fontSize: 28,
+        //                 fontWeight: FontWeight.w700,
+        //                 color: Colors.white),
+        //           ),
+        //         ],
+        //       ),
+        //     )),
+
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(Get.height * 0.1),
+          child: Container(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(
+                        image: const AssetImage("assets/logos/logoicon.ico"),
+                        height: Get.height * 0.1),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(30),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              LoginForm(),
+              FooterWidget(Texts: "Don't have an account ", Title: "Sign Up"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/commonScreens/splashscreen.dart
+++ lib/commonScreens/splashscreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'loginScreen.dart';
+import 'Login/loginScreen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,23 +10,26 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin
-
-{
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-    Future.delayed(const Duration(seconds: 2),(){
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder:(_)=>const LogingScreen(),
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (_) => const LogingScreen(),
       ));
     });
   }
+
   @override
   void dispose() {
     super.dispose();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
   }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
@@ -36,7 +39,8 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image(image: AssetImage('images/logo.png'),
+              Image(
+                image: AssetImage('images/logo.png'),
               ),
             ],
           ),
