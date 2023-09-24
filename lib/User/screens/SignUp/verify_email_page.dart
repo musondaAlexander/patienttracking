422450f31838f112903bdb586d10eef61f5bfda3
Alexander Musonda <74776587+musondaAlexander@users.noreply.github.com>
2023-09-24 13:01:35 +0200

    added the verify email page

---
+++ lib/User/screens/SignUp/verify_email_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// This Widget will be used to verify Email of the User.
+class VerifyEmailPage extends StatefulWidget {
+  const VerifyEmailPage({super.key});
+
+  @override
+  State<VerifyEmailPage> createState() => _VerifyEmailPageState();
+}
+
+class _VerifyEmailPageState extends State<VerifyEmailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
