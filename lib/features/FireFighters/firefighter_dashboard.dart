69e0da00a82c90e88c156da8e0bbd35610e36910
Alexander Musonda <74776587+musondaAlexander@users.noreply.github.com>
2023-09-28 13:24:14 +0200

    added new dependancies

---
+++ lib/features/FireFighters/firefighter_dashboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirefighterDashboard extends StatefulWidget {
+  const FirefighterDashboard({super.key});
+
+  @override
+  State<FirefighterDashboard> createState() => _FirefighterDashboardState();
+}
+
+class _FirefighterDashboardState extends State<FirefighterDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
