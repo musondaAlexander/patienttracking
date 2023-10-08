4a1db7ef166ecdcab82d3e54656325de5659f03c
Alexander Musonda <74776587+musondaAlexander@users.noreply.github.com>
2023-10-08 21:23:22 +0200

    upadated the Mylocation View

---
+++ lib/commonScreens/Location/shared_map_location_output.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyLocationMap extends StatefulWidget {
+  const MyLocationMap({super.key});
+
+  @override
+  State<MyLocationMap> createState() => _MyLocationMapState();
+}
+
+class _MyLocationMapState extends State<MyLocationMap> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
