529f897d022740f8c81c517df92c6523b35e16f9
Alexander Musonda <74776587+musondaAlexander@users.noreply.github.com>
2023-10-09 00:10:05 +0200

    change: updatd the Mapview for live tracking

--- lib/commonScreens/Location/shared_map_location_output.dart
+++ lib/commonScreens/Location/shared_map_location_output.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart' as loc;
+import 'package:patienttracking/commonScreens/Location/share_location.dart';
 
 class MyLocationMap extends StatefulWidget {
   final String userId;
@@ -9,8 +14,91 @@ class MyLocationMap extends StatefulWidget {
 }
 
 class _MyLocationMapState extends State<MyLocationMap> {
+  final loc.Location location = loc.Location();
+  late GoogleMapController _controller;
+  bool _added = false;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () => Get.to(const ShareMyLocation()),
+          child: const BackButton(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.lightBlueAccent,
+        title: const Center(
+            child: Text(
+          'Live Tracking',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        )),
+      ),
+      body: Stack(
+        children: <Widget>[
+          StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('location').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (_added) {
+                mymap(snapshot);
+              }
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return GoogleMap(
+                zoomControlsEnabled: false,
+                zoomGesturesEnabled: true,
+                scrollGesturesEnabled: true,
+                myLocationButtonEnabled: false,
+                mapType: MapType.normal,
+                markers: {
+                  Marker(
+                      position: LatLng(
+                        snapshot.data!.docs.singleWhere((element) =>
+                            element.id == widget.userId)['latitude'],
+                        snapshot.data!.docs.singleWhere((element) =>
+                            element.id == widget.userId)['longitude'],
+                      ),
+                      markerId: const MarkerId('id'),
+                      icon: BitmapDescriptor.defaultMarkerWithHue(
+                          BitmapDescriptor.hueMagenta)),
+                },
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(
+                      snapshot.data!.docs.singleWhere(
+                          (element) => element.id == widget.userId)['latitude'],
+                      snapshot.data!.docs.singleWhere((element) =>
+                          element.id == widget.userId)['longitude'],
+                    ),
+                    zoom: 14.47),
+                onMapCreated: (GoogleMapController controller) async {
+                  setState(() {
+                    _controller = controller;
+                    _added = true;
+                  });
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> mymap(AsyncSnapshot<QuerySnapshot> snapshot) async {
+    await _controller
+        .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+            target: LatLng(
+              snapshot.data!.docs.singleWhere(
+                  (element) => element.id == widget.userId)['latitude'],
+              snapshot.data!.docs.singleWhere(
+                  (element) => element.id == widget.userId)['longitude'],
+            ),
+            zoom: 14.47)));
   }
 }
